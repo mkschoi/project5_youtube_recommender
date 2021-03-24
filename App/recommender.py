5a63add18c724b10638398804edd65be6953e806
buildalpha0131 <72048581+buildalpha0131@users.noreply.github.com>
2021-03-23 23:08:59 -0400

    final changes to the app

--- App/recommender.py
+++ App/recommender.py
@@ -53,12 +53,12 @@ def initial_recommender(df, topic, duration, upload_date):
     
     return df_videos_filtered_all[['Video_ID','Title']].head(), df_videos_filtered_all, df_videos_filtered_topic
 
-def init_embedded_rec_videos(df):
-    init_recs_video_ids = list(df['Video_ID'])
-    init_recs_video_titles = list(df['Title'])
-    init_recs_ids_titles = list(zip(init_recs_video_ids, init_recs_video_titles))
+def embedded_rec_videos(df):
+    recs_video_ids = list(df['Video_ID'])
+    recs_video_titles = list(df['Title'])
+    recs_ids_titles = list(zip(recs_video_ids, recs_video_titles))
     
-    for id, title in init_recs_ids_titles:
+    for id, title in recs_ids_titles:
         st.subheader(title)
         st_player('www.youtube.com/watch?v=' + id)
         st.text("\n")
@@ -69,7 +69,7 @@ def follow_up_recommender(liked_video_list, df_videos_filtered):
     Output: Top five follow-up recommendations using content-based recommender system
     '''
     ## Fit and transform the transcript into a document-term matrix
-    word_list = [[word[0] for word in eval(doc)] for doc in df_videos_filtered['Transcript']]
+    word_list = [doc for doc in df_videos_filtered['Transcript']]
     vec = CountVectorizer(tokenizer=lambda doc:doc, lowercase=False)
     matrix = vec.fit_transform(word_list).toarray()
 
@@ -93,7 +93,7 @@ def follow_up_recommender(liked_video_list, df_videos_filtered):
     scores_df = pd.DataFrame(scores_list).T
     scores_df.columns = idx_list
 
-    ## Drop videos that were in the original recommendation
+    ## Drop videos that were in the initial recommendation
     scores_df.drop([0,1,2,3,4], inplace=True)
 
     ## Calculate the mean cosine similarity score for each video    
@@ -107,18 +107,8 @@ def follow_up_recommender(liked_video_list, df_videos_filtered):
     df_videos_follow_up_recs = df_videos_filtered.iloc[top_5_indices]
 
     return df_videos_follow_up_recs[['Video_ID','Title']]
-    
-def follow_up_embedded_rec_videos(df):
-    follow_up_recs_video_ids = list(df['Video_ID'])
-    follow_up_recs_video_titles = list(df['Title'])
-    follow_up_recs_ids_titles = list(zip(follow_up_recs_video_ids, follow_up_recs_video_titles))
+               
 
-    for id, title in follow_up_recs_ids_titles:
-        st.subheader(title)
-        st_player('www.youtube.com/watch?v=' + id)
-        st.text("\n")
-        
-        
 st.title('YouTube Video Recommender for Beginners in Stock Investing')
 
 st.subheader('_"The stock market is filled with individuals who know the price of everything, but the value of nothing."_ — Phillip Fisher')
@@ -141,14 +131,14 @@ st.markdown("If you are one of those people who recently started investing in st
          
 st.markdown("When you are buying a stock, you are not just trading a piece of paper. You are buying a piece of a company, a real _business_. And you are buying that because you believe it will grow revenues and generate a lot more cash flows than the market gives it credit for. Ultimately, a stock price reflects a company's _value_ in the long-term, and that value is determined based on how much cash flow its business will generate in the future, discounted all the way back to present.")
          
-st.markdown("Okay, if this sounds all foreign to you, you have come to the right place. I worked in the investment industry for eight years, but there are way better resources on stock investing than I can provide. Books on investing are the obvious first place to go to, but many people nowadays don't want to grab a book and pore into hundreds of pages of text (sadly). So as an alternative, a lot more people seek educational videos, which are more approachable, interactive, and fun. And what other place to look for content-rich, free-for-all videos than YouTube? That is why I have created a YouTube video recommender for those of you who want to get proper education on stock investing, including different investing styles, tools used in analyzing stocks, what makes a successful long-term investor, etc.")
+st.markdown("Okay, if this sounds all foreign to you, you have come to the right place. I worked in the investment industry for eight years, but there are way more resources on stock investing than I can provide. Books on investing are the obvious first place to go to, but many people nowadays don't want to grab a book and pore into hundreds of pages of text (sadly). So as an alternative, they seek educational videos, which are more approachable, interactive, and fun. And what other place to look for content-rich, free-for-all videos than YouTube? That is why I have created a YouTube video recommender for those of you looking to get proper education on stock investing, including different investing styles, tools used in analyzing stocks, what makes a successful long-term investor, etc.")
          
-st.markdown("You might ask, 'Why do I need your recommender? I can just go on YouTube and find the videos myself!' Well, the reason is simple. As a beginner in stock investing, you are unlikely to be able to distinguish good, educational videos from bad, promotional, and often misleading videos.  And there are A LOT of the latter on YouTube, based on my own experience. So without further ado, let's start your investment journey!")
+st.markdown("You might ask, 'Why do I need your recommender? I can just go on YouTube and find the videos myself!' Well, the reason is simple. As a beginner in stock investing, you are unlikely to be able to distinguish good, educational videos from bad, promotional, and often misleading videos.  And there are A LOT of the latter on YouTube, based on my own experience. So without further ado, let's begin your investment journey!")
 
 st.text("\n")
 toc.header("Topics to Explore")
 
-st.markdown("The videos are categorized into 12 different topics on investing. Below are the brief descriptions of the topics and why they are important to learn as a beginner. You don't have to go from the top to bottom, but I have ordered them in a logical sequence so that you learn the basics of different investment styles before delving into the tools used in analyzing stocks.")
+st.markdown("The videos are categorized into nine different topics on investing. Below are brief descriptions of the topics and why they are important to learn as a beginner. You don't have to go from the top to bottom, but I have ordered them in a logical sequence so that you learn the basics of different investment styles before delving into the tools used in analyzing stocks.")
 
 st.markdown("**Value Investing**: An investment strategy that involves picking stocks that appear to be trading for less than their intrinsic value. In the traditional sense, a value investor cares more about the value of a company's assets on the balance sheet and its current earnings power than its future growth. Benjamin Graham (the father of value investing) first institutionalized the concept in the 1930s, and has since become a pillar for other styles of investing. Naturally, a beginner looking to become a proper investor would want to understand the concept of value investing as his/her first stepping stone.")
 st.markdown("**Growth Investing**: Growth investing is an investment style/strategy that is focused on seeking companies that offer rapid revenue and earnings growth into the future, with asset values and the current earnings power taking a backseat. However, many investors view growth investing to be part of value investing, because growth is simply a component of value. It is important to learn why the distinction could get fuzzy.")
@@ -157,7 +147,7 @@ st.markdown("**Long-term Investing**: This is not a separate investment style, b
 # st.markdown("**Dividend Investing**: A strategy of buying stocks that pay periodic dividends in order to receive a regular income from your investments. A company has discretion over whether to pay a dividend out of its profit, and it is considered _after_ making all the internal investments needed to grow its business. Therefore, dividend stocks are typically found among stable, mature companies/industries generating ample excess cash flow (e.g., Consumer Staples, Utilities, and Telecom)")
 st.markdown("**Fundamental vs. Technical Analysis**: Two major schools of thought or approaches to making money in the stock market. Fundamental analysis involves studying a company's business operations, financial statements, and competitive/macro landscape in order to derive its intrinsic value (all the investment styles mentioned above require at least some fundamental analysis). Technical analysis is different in that _traders_ attempt to identify opportunities by studying a stock's price/volume charts and finding patterns and trends in the short-term price movements. Technical analysis can be an ancillary component of an investor's stock analysis, but it should never be the main driver behind his/her investment thesis.")
 st.markdown("**Economic Moats**: An economic moat is a distinct, sustainable advantage a company has over its competitors which allows it to protect its market share and profitability (like moats around a castle to protect against an invasion). It is often an advantage that is difficult to mimic or duplicate (brand identity, patents, network effects). Economic moats are one of the most important qualities that an investor should look for in a company when performing fundamental analysis with a long-term mindset.")
-st.markdown("**Valuation**: Valuation is an analytical process of determining the fair value of an asset or a company. There are many ways to valuing a company depending on which industry it is in, whether it is generating positive or negative cash flow, etc (many of the YouTube videos here delve into these different approaches). If finding economic moats is the ultimate goal of a qualitative analysis, valuation is the final quantitative output you want to get to compare to the company's current stock price, and determine if it is buy, hold, or a sell.")
+st.markdown("**Valuation**: Valuation is an analytical process of determining the fair value of an asset or a company. There are many ways to valuing a company depending on which industry it is in, whether it is generating positive or negative cash flow, etc (many of the YouTube videos here delve into these different approaches). If finding economic moats is the ultimate goal of a qualitative analysis, valuation is the final quantitative output you arrive at to compare it to the company's current stock price, and determine if it is a buy, hold, or a sell.")
 # st.markdown("**Valuation (Case Studies)**: Valuation methods are hard to grasp with just theories and simple examples. Conducting case studies and walking through each step of a valuation process is helpful in cementing the logic behind how an intrinsic value is determined. A properly educated investor should have the skills to correctly use different valuation methods under varying situations, and as a result, have the conviction to make big investment decisions")
 st.markdown("**Technology Stocks**: Over the past decade, the performance of the stock market has mostly been driven by the so-called 'mega-cap tech stocks' or 'FAANGs'. It is important to study why these companys have outperformed for so long, what their differentiating qualities (or moats) are, and whether it is sustainable.")
 # st.markdown("**Electric Vehicle Stocks**: One industry that has caught the investment world's attention over the last few years, is the electric vehicle industry, led by Tesla. While there are no doubts as to the fact that much of the meteoric rise in Tesla's stock price is euphoria-driven, it is still important to understand why the industry has gained so much popularity among not only retail but institutional investors, what the long-term trends are, and how one should value a company in this hyper-growth but cash flow-shy sector.")
@@ -199,7 +189,7 @@ if session_state.search_button_init:
     df_videos_filtered = initial_recommender(df_videos_cleaned_v10, topic, duration, upload_date)[1]
     df_videos_filtered_topic = initial_recommender(df_videos_cleaned_v10, topic, duration, upload_date)[2]
     df_videos_other = pd.concat([df_videos_filtered.head(), df_videos_cleaned_v10.drop(df_videos_filtered_topic.index)])
-    init_embedded_rec_videos(df_videos_recs_init)
+    embedded_rec_videos(df_videos_recs_init)
     
     st.text("\n")
     toc.header('Follow-up Video Recommender')
@@ -209,19 +199,19 @@ if session_state.search_button_init:
     st.markdown("**Which video(s) did you like?**")
     
     titles = list(df_videos_recs_init['Title'])
-    session_state.video_id_follow_up = st.multiselect('Select one or more:', options=['', titles[0], titles[1], titles[2], titles[3], titles[4]])
+    video_id_follow_up = st.multiselect('Select one or more:', options=['', titles[0], titles[1], titles[2], titles[3], titles[4]])
     
-    session_state.cross_intra_topic = st.radio('Select one:', ('Intra-topic', 'Cross-topic'))
+    cross_intra_topic = st.radio('Select one:', ('Intra-topic', 'Cross-topic'))
     
-    if session_state.video_id_follow_up:
-        session_state.search_button_follow_up = st.button('Search for recommended videos', key=2)
-        if session_state.search_button_follow_up:
-            if session_state.cross_intra_topic=='Intra-topic':
-                df_videos_recs_follow_up = follow_up_recommender(session_state.video_id_follow_up, df_videos_filtered)
-                follow_up_embedded_rec_videos(df_videos_recs_follow_up)
+    if video_id_follow_up: 
+        search_button_follow_up = st.button('Search for recommended videos', key=2)
+        if search_button_follow_up:
+            if cross_intra_topic=='Intra-topic':
+                df_videos_recs_follow_up = follow_up_recommender(video_id_follow_up, df_videos_filtered)
+                embedded_rec_videos(df_videos_recs_follow_up)
             else:
-                df_videos_recs_follow_up = follow_up_recommender(session_state.video_id_follow_up, df_videos_other)
-                follow_up_embedded_rec_videos(df_videos_recs_follow_up)
+                df_videos_recs_follow_up = follow_up_recommender(video_id_follow_up, df_videos_other)
+                embedded_rec_videos(df_videos_recs_follow_up)
 
 toc.generate()
 
